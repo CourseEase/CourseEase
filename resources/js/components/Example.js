26a31340d2e8db82992dda5bc101a2510fde7973
Mezmerizingcoder <mezmerizingcoder444@gmail.com>
2023-05-29 20:07:24 +0800

    Added Button Styles

--- resources/js/components/Example.js
+++ resources/js/components/Example.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import ButtonStyle1 from './Buttons/ButtonStyle1';
 
 function Example() {
     return (
@@ -9,7 +10,10 @@ function Example() {
                     <div className="card">
                         <div className="card-header">Example Component</div>
 
-                        <div className="card-body">I'm an example component!</div>
+                        <div className="card-body">
+                            <ButtonStyle1 looks="style-1">Style 1</ButtonStyle1>
+                            <ButtonStyle1 looks="style-2">Style 2</ButtonStyle1>
+                        </div>
                     </div>
                 </div>
             </div>
