b588c1d5eaab4aeb36b748c9f234d0dda9671b69
Mezmerizingcoder <mezmerizingcoder444@gmail.com>
2023-05-30 16:14:21 +0800

    Added Title Field Comp

--- resources/js/app.js
+++ resources/js/app.js
@@ -15,3 +15,5 @@ require('./bootstrap');
 require('./components/Example');
 
 require('./components/Buttons/ButtonStyle1');
+
+require('./components/Home');
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
+<!-- <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
@@ -21,7 +21,8 @@
             </div>
         </div>
     </div>
-</div>
+</div> -->
 
-<div id="example"></div>
+<!-- <div id="example"></div> -->
+<div id="home"></div>
 @endsection
