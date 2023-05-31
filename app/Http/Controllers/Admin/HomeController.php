29233881585558cdc483858b2e466d01229c2ecc
Mezmerizingcoder <mezmerizingcoder444@gmail.com>
2023-05-31 16:25:46 +0800

    Header update

--- app/Http/Controllers/Admin/HomeController.php
+++ app/Http/Controllers/Admin/HomeController.php
@@ -12,4 +12,8 @@ class HomeController extends Controller
     {
         return view("admin/home");
     }
+
+    public function createLesson(){
+        return view("admin/create-lesson");
+    }
 }
